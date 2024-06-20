fd6bbe7ad28f1abe27c49b1a4e7e3fa14c0299e7
Balo Andor <balo.andor@xpericad.hu>
2024-06-21 00:18:34 +0200

    created menu

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+void welcomeScreen(){
+    cout << "##########################################################################" << endl;
+    cout << "#                  Welcome to Console Mini Games!                        #" << endl;
+    cout << "##########################################################################" << endl;
+}
+
+char menuScreen(){
+    char choice;
+    cout << " Please choice from the menu:" << endl;
+    cout << " X - XoX" << endl;
+    cout << " M - Mastermind" << endl;
+    cout << " R - Rock - Paper - Scissors" << endl;
+    cout << " A - About" << endl;
+    cout << " E - Exit" << endl;
+    cout << " Your choice: ";
+    cin >> choice;
+
+    return choice;
+}
+
+char normalizeInput(char input){
+    if( input >= 65 && input <= 90){
+        return input + 32;
+    }
+    else{
+        return input;
+    }
+}
+
+int main(){
+    welcomeScreen();
+    char choice = menuScreen();
+
+    switch(normalizeInput(choice)){
+        case 'x':
+            break;
+        case 'm':
+            break;
+        case 'r':
+            break;
+        case 'a':
+            break;
+        case 'e':
+            break;
+        default:
+            break;
+    }
+
+    return 0;
+}
